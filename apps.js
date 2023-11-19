174670216e9507b6f41878b40a2d962594e49d1b
Vyybor <vyybor@gmail.com>
2023-11-19 18:32:55 -0500

    first commit

---
+++ apps.js
@@ -0,0 +1,25 @@
+const password = document.getElementById("signup--password");
+const confirmPassword = document.getElementById("signup--confirm-password");
+
+let isModalOpen = false;
+
+function validatePassword() {
+  if (password.value != confirmPassword.value) {
+    confirmPassword.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+password.onkeyup = validatePassword;
+confirmPassword.onkeyup = validatePassword;
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
